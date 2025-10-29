20211ed2f1f77fc475077b31866136f642a66533
Nurdin <mrsunnysummer@gmail.com>
2025-10-29 11:01:45 +0700

    style: add preloader and remove unused assets

--- app/Views/home.php
+++ app/Views/home.php
@@ -16,6 +16,15 @@
 </head>
 
 <body>
+    <!-- Preloader -->
+    <div id="preloader">
+        <div class="preloader-inner">
+            <div class="spinner">
+                <img src="../assets/img/logo.png" alt="img">
+            </div>
+            <h2 class="preloader-text">Portal SIPASTI</h2>
+        </div>
+    </div>
     <!-- Navigation -->
     <nav class="navbar">
         <div class="container-fluid">
@@ -378,6 +387,47 @@
             pattern.style.backgroundPosition = `${x}px ${y}px`;
         });
     </script>
+
+    <!-- Core JS -->
+    <script src="../assets/js/jquery-3.6.0.min.js"></script>
+
+    <!-- Framework -->
+    <script src="../assets/js/bootstrap.min.js"></script>
+
+    <!-- WOW Scroll Effect -->
+    <script src="../plugins/wow/wow.min.js"></script>
+
+    <!-- Swiper Slider -->
+    <script src="../plugins/swiper/swiper-bundle.min.js"></script>
+    <script src="../plugins/swiper/swiper-gl.min.js"></script>
+
+    <!-- Odometer Counter -->
+    <script src="../plugins/odometer/appear.js"></script>
+    <script src="../plugins/odometer/odometer.js"></script>
+
+    <!-- Projects -->
+    <script src="../plugins/isotope/isotope.pkgd.min.js"></script>
+    <script src="../plugins/isotope/imagesloaded.pkgd.min.js"></script>
+
+    <script src="../plugins/isotope/tilt.jquery.js"></script>
+    <script src="../plugins/isotope/isotope-init.js"></script>
+
+    <!-- Fancybox -->
+    <script src="../plugins/fancybox/jquery.fancybox.min.js"></script>
+
+    <!-- Flatpickr -->
+    <script src="../plugins/flatpickr/flatpickr.min.js"></script>
+
+    <!-- Nice Select -->
+    <script src="../plugins/nice-select/jquery.nice-select.min.js"></script>
+
+
+
+    <!-- Cursor Effect -->
+    <script src="../plugins/cursor-effect/cursor-effect.js"></script>
+
+    <!-- Theme Custom JS -->
+    <script src="../assets/js/theme.js"></script>
 </body>
 
 </html>
